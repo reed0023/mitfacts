d6e350c788dd7fb0610e69e660f78d8d44bc9909
reed0023 <stephenreedpgh@gmail.com>
2021-07-28 15:16:20 -0400

    final fixes

--- mit.js
+++ mit.js
@@ -1,7 +1,7 @@
 var mit = {
     city: 'Cambridge',
-    colors: ['Silver', 'Gray'],
+    colors: 'Silver Gray',
     mascot: "robot",
     founded: 1861,
-    motto: 'Carpe diem',
+    motto: 'Mens',
 };
